19f8a62003d3591a2a5ea52f93bf9bd0de9d50a2
claudianjeri <claudianjeri04@gmail.com>
2018-07-18 18:33:31 +0300

    added all html templates

--- decor/splendor/urls.py
+++ decor/splendor/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$', views.home, name='home'),
-    
-
+    url('index.html', views.home, name='home'),
+    url('shop.html', views.shop, name='shop'),
+    url('services.html', views.services, name='services'),
+    url('cart.html', views.cart, name='cart'),
+    url('product-details.html', views.product, name='product'),
+    url('404.html', views.fourowfour, name='fourowfour'),
+    url('checkout.html', views.checkout, name='checkout')
 ]
--- decor/splendor/views.py
+++ decor/splendor/views.py
@@ -5,3 +5,22 @@ from django.http  import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def product(request):
+    return render(request, 'product-details.html')
+
+def fourowfour(request):
+    return render(request, '404.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
